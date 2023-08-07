ed7d67dac61b38be99e1fb84c1e504c86babff90
叶浩 <77729052@qq.com>
2023-08-07 18:35:21 +0800

    refactor: http proxy

--- api/notion.ts
+++ api/notion.ts
@@ -1,12 +1,20 @@
 import axios from 'axios'
 
-export async function getDatabaseData() {
-    return axios.post('https://api.notion.com/v1/databases/dac877360d264a8a86a8da23eb2de5a7/query', {}, {
-        proxy: {
+const proxy = () => {
+    if (process.env.APP_ENV === 'dev') {
+        return {
             protocol: 'http',
             host: '127.0.0.1',
             port: 7890,
-        },
+        }
+    } else {
+        return undefined
+    }
+}
+
+export async function getDatabaseData() {
+    return axios.post('https://api.notion.com/v1/databases/dac877360d264a8a86a8da23eb2de5a7/query', {}, {
+        proxy: proxy(),
         headers: {
             'Authorization': 'Bearer secret_gGr9EzrWixlO2Fn96tqxGtuYDYr0zXAIWLjtyh0ndz0',
             'Notion-Version': '2022-06-28'
@@ -28,11 +36,7 @@ export async function getDatabaseData() {
 
 export async function listPageBlock(id: string) {
     return axios.get(`https://api.notion.com/v1/blocks/${id}/children`, {
-        proxy: {
-            protocol: 'http',
-            host: '127.0.0.1',
-            port: 7890,
-        },
+        proxy: proxy(),
         headers: {
             'Authorization': 'Bearer secret_gGr9EzrWixlO2Fn96tqxGtuYDYr0zXAIWLjtyh0ndz0',
             'Notion-Version': '2022-06-28'
