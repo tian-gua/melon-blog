644a0cdde9d6296998f9bff17987701eed9eb887
叶浩 <77729052@qq.com>
2023-12-21 19:46:20 +0800

    feat: 优化toc

--- components/toc.tsx
+++ components/toc.tsx
@@ -24,27 +24,35 @@ const Toc = (props: { id: string, data: TocData }) => {
         })
 
         window.addEventListener('scroll', (event) => {
+            // 如果滑到了底部,就把最后一个id设置为active
+            const bottom = document.documentElement.scrollHeight - document.documentElement.clientHeight
+            if (window.scrollY >= bottom) {
+                setActiveId(props.data.children[props.data.children.length - 1].id)
+            }
+
+            const distances: { id: string, distance: number }[] = []
             const headerOffset = 30;
             props.data.children.forEach((child) => {
                 const header = document.getElementById(child.id)!
                 const headerTopPosition = header.getBoundingClientRect().top;
                 const offsetPosition = headerTopPosition + window.scrollY - headerOffset;
-
                 // console.log(`${child.title} ${offsetPosition} ${window.scrollY} ${window.scrollY - offsetPosition}`)
-
-                if (Math.abs(window.scrollY - offsetPosition) <= 10) {
+                const distance = Math.abs(window.scrollY - offsetPosition)
+                distances.push({id: child.id, distance: distance})
+                if (distance <= 10) {
                     if (activeId !== child.id) {
                         setActiveId(child.id)
+                        return
                     }
-                    return
                 }
             })
 
-            // check if scroll to bottom
-            const bottom = document.documentElement.scrollHeight - document.documentElement.clientHeight
-            if (window.scrollY >= bottom) {
-                setActiveId(props.data.children[props.data.children.length - 1].id)
-            }
+
+            // 如果滑动过快,可能会监听不到当前真实的id,所以这里做一下排序,取距离最小的那个,非常增加丝滑度
+            distances.sort((a, b) => {
+                return a.distance - b.distance
+            })
+            setActiveId(distances[0].id)
         })
     }, [props.id]);
 
