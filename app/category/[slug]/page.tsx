1009379a47788f02fc5fac8ce428e5f0e17796f8
叶浩 <77729052@qq.com>
2023-12-27 16:04:57 +0800

    refactor: 调整导航

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import notionService from "@/server/service/notion-service";
+import Tags from "@/components/tags";
 
 export default async function ArticleList({params}: { params: { slug: string } }) {
     const categoryBlogs: Blog[] | undefined = await notionService.getCategoryBlogs(decodeURIComponent(params.slug))
+    const tags = await notionService.getBlogTags()
     return <div className="text-base antialiased">
         <h1 className="w-full h-full px-3 font-extrabold text-[1.5em] mb-5">
             {decodeURIComponent(params.slug).split('-')[1]}
@@ -13,6 +15,7 @@ export default async function ArticleList({params}: { params: { slug: string } }
                             className="w-full h-10 px-3 flex justify-end items-center hover:bg-gray-100 rounded-md">
                     <a className="grow"
                        href={`/blog/${blog.id}/${blog.title}`}>{blog.title}</a>
+                    <Tags tags={tags.get(blog.id)}/>
                     <span className="text-gray-400">{blog.date}</span>
                 </div>
             })
--- components/navigator.tsx
+++ components/navigator.tsx
@@ -3,11 +3,10 @@ import React from "react";
 const Navigator = () => {
     return <>
         <div className="h-16 w-auto flex">
-            <div className="h-full w-52 grid content-center">
+            <div className="h-full w-40 grid content-center">
                 <div
                     className="h-10 flex justify-between items-center px-8 text-sm font-bold text-gray-600">
                     <a href="/">首页</a>
-                    <a href="/">博客</a>
                     <a href="/">作品集</a>
                 </div>
             </div>
