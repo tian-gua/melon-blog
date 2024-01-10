90400629e63321a5180a69832c8933acb8e3c016
叶浩 <77729052@qq.com>
2024-01-10 12:39:57 +0800

    feat: 优化quote样式

--- server/renderer/notion/quote-renderer.tsx
+++ server/renderer/notion/quote-renderer.tsx
@@ -8,7 +8,7 @@ class QuoteRenderer extends BaseRenderer implements Renderer {
         }
 
         return <blockquote
-            className="p-4 my-4 border-l-4 border-[#d9d9d9] bg-[#f8f8f8] dark:border-gray-500 dark:bg-gray-800">
+            className="p-4 my-4 border-l-4 border-gray-700 bg-gray-100 dark:border-gray-500 dark:bg-gray-800">
             <p className="text-[0.9em] leading-relaxed text-gray-900 dark:text-white">{block.quote.rich_text.map((richText, index) => {
                 return <span className="" key={index}
                              style={this.processAnnotation(richText.annotations)}>{richText.plain_text}</span>
