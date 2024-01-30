8bbfe752dfbd7f63182964156e3b7d384fd45eac
叶浩 <77729052@qq.com>
2024-01-30 18:57:31 +0800

    调整简介

--- components/description.tsx
+++ components/description.tsx
@@ -6,16 +6,12 @@ import {TypeAnimation} from "react-type-animation";
 const Description = () => {
     return <>
         <div className="w-full h-max px-3">
-            <h1 className="flex flex-col text-xl leading-normal font-serif">
-                {/*<span className="">Hello, this is melon&lsquo;s blog</span>*/}
+            <h1 className="flex flex-col text-[50px] leading-normal font-['FasterOne-Regular'] text-black">
                 <TypeAnimation
-                    className="text-[#a2a2a2]"
                     sequence={[
                         // Same substring at the start will only be typed out once, initially
-                        `Hello, I'm Melon, this is Melon's blog`,
+                        `Hello, I'm Melon`,
                         2000, // wait 1s before replacing "Mice" with "Hamsters"
-                        "Thank you for visiting my blog",
-                        2000
                     ]}
                     wrapper="span"
                     speed={50}
@@ -23,10 +19,9 @@ const Description = () => {
                 />
             </h1>
             <div className="mt-2 text-gray-600 antialiased text-[0.9em]">
-                <p className="flex flex-wrap py-1"><span>🧑‍💻&nbsp;全栈开发者&nbsp;/&nbsp;</span><span>Full-stack Developer</span>
+                <p className="flex flex-wrap py-1"><span>🧑‍💻&nbsp;Java开发者&nbsp;/&nbsp;全栈开发者&nbsp;/&nbsp;</span><span> Full-stack Developer</span>
                 </p>
-                {/*<p className="flex flex-wrap py-1">🤩&nbsp;正在做一些有趣的事 / Working on something interesting</p>*/}
-                <p className="flex flex-wrap py-1">🥰&nbsp;我正在经营一个研发工作室,如果您有项目需要合作,可以联系我</p>
+                <p className="flex flex-wrap py-1">🤩&nbsp;正在做一些有趣的事 / Working on something interesting</p>
             </div>
         </div>
     </>
--- components/navigator.tsx
+++ components/navigator.tsx
@@ -8,7 +8,7 @@ const Navigator = () => {
             <a className={"flex justify-start items-center"} href={"/"}>
                 <img className={"w-10 h-10 mr-2 rounded-full"}
                      src={"/M.png"}/>
-                <h1 className={"font-serif font-bold text-[20px]"}>{'MelonBlog'}</h1>
+                <h1 className={"fonts-serif fonts-bold text-[20px]"}>{'MelonBlog'}</h1>
             </a>
             <div className={"flex-1"}></div>
             <div className={"flex justify-center items-center"}>
