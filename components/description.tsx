838a01c5eb503fdeb36b2d493131aa593bcdb69b
叶浩 <77729052@qq.com>
2025-07-02 16:04:47 +0800

    chore: 调整简介

--- components/description.tsx
+++ components/description.tsx
@@ -1,31 +1,38 @@
-'use client'
+"use client";
 
-import {TypeAnimation} from "react-type-animation";
-
+import { TypeAnimation } from "react-type-animation";
 
 const Description = () => {
-    return <>
-        <div className="w-full h-max px-3">
-            <h1 className="flex flex-col text-[50px] leading-normal font-['Courgette-regular'] text-black">
-                <TypeAnimation
-                    sequence={[
-                        // Same substring at the start will only be typed out once, initially
-                        `Hello, I'm Melon`,
-                        2000, // wait 1s before replacing "Mice" with "Hamsters"
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    repeat={Infinity}
-                    cursor={true}
-                />
-            </h1>
-            <div className="mt-2 text-gray-600 antialiased text-[0.9em]">
-                <p className="flex flex-wrap py-1"><span>🧑‍💻&nbsp;Java开发者&nbsp;/&nbsp;全栈开发者&nbsp;/&nbsp;</span><span> Full-stack Developer</span>
-                </p>
-                <p className="flex flex-wrap py-1">🤩&nbsp;正在做一些有趣的事 / Working on something interesting</p>
-            </div>
+  return (
+    <>
+      <div className="w-full px-3 h-max">
+        <h1 className="flex flex-col text-[50px] leading-normal font-['Courgette-regular'] text-black">
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              `Hello, I'm Melon`,
+              2000, // wait 1s before replacing "Mice" with "Hamsters"
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+            cursor={true}
+          />
+        </h1>
+        <div className="mt-2 text-gray-600 antialiased text-[0.9em]">
+          <p className="flex flex-wrap py-1">
+            <span>
+              🧑‍💻&nbsp;Java&Go&Python开发者&nbsp;/&nbsp;全栈开发者&nbsp;/&nbsp;
+            </span>
+            <span> Full-stack Developer</span>
+          </p>
+          <p className="flex flex-wrap py-1">
+            🤩&nbsp;正在做一些有趣的事 / Working on something interesting
+          </p>
         </div>
+      </div>
     </>
-}
+  );
+};
 
 export default Description;
